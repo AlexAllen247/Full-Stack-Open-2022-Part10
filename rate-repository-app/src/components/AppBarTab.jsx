8a192ab8fb9c410374235cf74b96584db940a8ca
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-12-17 16:38:07 +0100

    completed exercises 10.6-10.7

--- rate-repository-app/src/components/AppBarTab.jsx
+++ rate-repository-app/src/components/AppBarTab.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Pressable } from "react-native";
 import Constants from "expo-constants";
 import Text from "./Text";
 import theme from "../theme";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   text: {
@@ -11,11 +12,13 @@ const styles = StyleSheet.create({
   },
 });
 
-const AppBarTab = ({ tab }) => {
+const AppBarTab = ({ path, tab }) => {
   return (
+    <Link to={path}>
     <Pressable>
       <Text style={styles.text}>{tab}</Text>
     </Pressable>
+    </Link>
   );
 };
 
