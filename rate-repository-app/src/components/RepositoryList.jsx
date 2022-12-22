e50f4a0bc23973269f13bbd56a655704519bfa2c
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-12-22 13:21:32 +0100

    completed exercise 10.23

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -2,16 +2,38 @@ import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useNavigate } from "react-router-native";
+import { useState } from "react";
+import Text from "./Text";
+import theme from "../theme";
+import { Picker } from "@react-native-picker/picker";
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  sortPicker: {
+    height: 50,
+    width: "80%",
+  },
+  sortByText: {
+    fontSize: theme.fontSizes.body,
+    fontWeight: theme.fontWeights.bold,
+  },
+  sortByContainer: {
+    flexDirection: "row",
+    margin: 10,
+    padding: 5,
+    backgroundColor: "lightgrey",
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({
+  repositories,
+  sortBy,
+  setSortBy,
+}) => {
   const navigate = useNavigate();
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
@@ -25,20 +47,52 @@ export const RepositoryListContainer = ({ repositories }) => {
     );
   };
 
+  const SortComponent = ({ sortBy, setSortBy }) => {
+    return (
+      <View style={styles.sortByContainer}>
+        <View style={{ justifyContent: "center", height: 50 }}>
+          <Text style={styles.sortByText}>Order By</Text>
+        </View>
+        <Picker
+          selectedValue={sortBy}
+          style={styles.sortPicker}
+          onValueChange={(itemValue) => setSortBy(itemValue)}
+        >
+          <Picker.Item label="Latest Repositories" value="latest" />
+          <Picker.Item
+            label="Highest rated Repositories"
+            value="highestRated"
+          />
+          <Picker.Item label="Lowest rated Repositories" value="lowestRated" />
+        </Picker>
+      </View>
+    );
+  };
+
   return (
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={viewItem}
       keyExtractor={(item) => item.id}
+      ListHeaderComponent={() => (
+        <SortComponent sortBy={sortBy} setSortBy={setSortBy} />
+      )}
     />
   );
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [sortBy, setSortBy] = useState("latest");
+  const { repositories } = useRepositories(sortBy);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      sortBy={sortBy}
+      setSortBy={setSortBy}
+    />
+  );
 };
 
 export default RepositoryList;
--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -2,10 +2,28 @@ import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const convertSortBy = (sortBy) => {
+  if (!sortBy)
+    return null;
+
+  switch (sortBy) {
+    case 'latest':
+      return ({ orderBy: 'CREATED_AT', orderDirection: 'DESC' });
+    case 'highestRated':
+      return ({ orderBy: 'RATING_AVERAGE', orderDirection: 'DESC' });
+    case 'lowestRated':
+      return ({ orderBy: 'RATING_AVERAGE', orderDirection: 'ASC' });
+    default:
+      return ({});
+  }
+};
+
+const useRepositories = (sortBy) => {
+  const variables = convertSortBy(sortBy);
   const [repositories, setRepositories] = useState();
 
   const { loading, error, data, refetch } = useQuery(GET_REPOSITORIES, {
+    variables,
     fetchPolicy: "cache-and-network",
   });
 
